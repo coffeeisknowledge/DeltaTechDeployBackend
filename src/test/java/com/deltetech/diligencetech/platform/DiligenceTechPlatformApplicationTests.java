b8854a355d2ccbce23ae9331d2826114335bbbb2
Abel Angel Ortega <abelangel2001@outlook.com>
2024-06-09 19:41:35 -0500

    test

--- src/test/java/com/deltetech/diligencetech/platform/DiligenceTechPlatformApplicationTests.java
+++ src/test/java/com/deltetech/diligencetech/platform/DiligenceTechPlatformApplicationTests.java
@@ -1,4 +1,4 @@
-package com.deltatech.diligencetech.platform;
+package com.deltetech.diligencetech.platform;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
